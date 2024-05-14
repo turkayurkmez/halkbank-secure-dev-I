f4d02b09709012c9805583ec3bcfb2e709c3d853
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-14 11:03:10 +0300

    AuthN and AuthZ  process

---
+++ AuthNZ/AuthNZ/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using AuthNZ.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthNZ.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegisterUser()
+        {
+            //bu action'dan sadece "müşteri" rolüne dahil olunabilir.
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterUser(UserRegisterViewModel user) 
+        {
+            //bu action'dan sadece "müşteri" rolüne dahil olunabilir.
+            if (ModelState.IsValid)
+            {
+                return View("Success");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ AuthNZ/AuthNZ/Models/UserRegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthNZ.Models
+{
+    public class UserRegisterViewModel
+    {
+        //YAGNI: You ain't gonna need it!
+        //Yak Shaving: Gereksiz işlemleri (ya da özellikleri) traşla
+
+        [Required(ErrorMessage = "Kullanıcı adı boş olamaz")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+      
+
+
+    }
+}
